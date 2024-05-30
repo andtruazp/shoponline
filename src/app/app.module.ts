73e71d850cbc7edbc2473da7d005c8566e8e4d46
neazp <andtruazp@gmail.com>
2024-05-29 22:18:33 -0600

    branch prod

---
+++ src/app/app.module.ts
@@ -0,0 +1,27 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { PrimengModule } from './primeng.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './componentes/navbar/navbar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    PrimengModule,
+    AppRoutingModule
+    
+  ],
+  providers: [
+    provideClientHydration()
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/primeng.module.ts
@@ -0,0 +1,29 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ToolbarModule } from 'primeng/toolbar';
+import { MenubarModule } from 'primeng/menubar';
+import { ButtonModule } from 'primeng/button';
+import { AvatarModule } from 'primeng/avatar';
+import { TooltipModule } from 'primeng/tooltip';
+import { MenuModule } from 'primeng/menu';
+
+const modPrime: any = [
+  ToolbarModule,
+  MenuModule,
+  MenubarModule,
+  ButtonModule,
+  AvatarModule,
+  TooltipModule
+];
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    modPrime
+  ],
+  exports:[
+    modPrime
+  ]
+})
+export class PrimengModule { }
