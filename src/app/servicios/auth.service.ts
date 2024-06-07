074a02065cd2bf88dfc2dd146536f8b4ae92e154
neazp <andreatrujilloazp@gmail.com>
2024-06-07 12:09:13 -0600

    funcionalidad login

--- src/app/servicios/auth.service.ts
+++ src/app/servicios/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { User } from '../interfaces/auth';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,6 +14,10 @@ export class AuthService {
 
   registerUser(usuario: User){
     return this.http.post(`${this.baseUrl}/users`,usuario);
+  }
+
+  getUserByEmail(email:string):Observable<User[]> {
+    return this.http.get<User[]>(`${this.baseUrl}/users?email=${email}`)
   }
 
 }
